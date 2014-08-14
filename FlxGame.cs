7a44f01e08e59565fef8492288cc162b27131549
Earl Baucum <earl.baucum@att.net>
2014-08-14 14:06:12 -0400

    reduce usings

--- FlxGame.cs
+++ FlxGame.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using System.Windows;
-using System.Windows.Shapes;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using org.flixel.data;
 
